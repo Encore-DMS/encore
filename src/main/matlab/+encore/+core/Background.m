8715237ea637922f9fbd4afc0f833744a48b304d
Mark Cafaro <mark.cafaro@gmail.com>
2017-06-01 15:11:59 -0700

    Implemented background properties

--- src/main/matlab/+encore/+core/Background.m
+++ src/main/matlab/+encore/+core/Background.m
@@ -1,12 +1,27 @@
 classdef Background < encore.core.Signal
     
     properties
+        value
+        sampleRate
+        sampleRateUnits
     end
     
     methods
         
         function obj = Background(cobj)
             obj@encore.core.Signal(cobj);
+        end
+        
+        function v = get.value(obj)
+            v = double(obj.cobj.getValue());
+        end
+        
+        function r = get.sampleRate(obj)
+            r = double(obj.cobj.getSampleRate());
+        end
+        
+        function u = get.sampleRateUnits(obj)
+            u = char(obj.cobj.getSampleRateUnits());
         end
         
     end
